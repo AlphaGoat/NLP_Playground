9d4f9d3c39a747e1a38a52985654dab2af9bfd25
Peter Thomas <pjtpeterjohnthomas@gmail.com>
2019-06-07 19:39:00 -1000

    Added pointwise feed forward function

--- ann.py
+++ ann.py
@@ -69,6 +69,30 @@ class Model(object):
 
         assert d_model % num_heads == 0
 
+    def variable_summaries(self, var):
+        """Attach a lot of summaries to a Tensor
+        (for TensorBoard visualization)."""
+
+        with tf.name_scope('summaries'):
+
+            mean = tf.reduce_mean(var)
+
+            tf.summary.scalar('mean', mean)
+
+            with tf.name_scope('stddev'):
+
+                stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+
+            tf.summary.scalar('stddev', stddev)
+
+            tf.summary.scalar('max', tf.reduce_max(var))
+
+            tf.summary.scalar('min', tf.reduce_min(var))
+
+            tf.summary.histogram('histogram', var)
+
+        return
+
     def weight_variable(self, shape):
 
         initial = tf.truncated_normal(shape, stddev=0.1)
@@ -83,6 +107,32 @@ class Model(object):
 
     def feed_forward_layer(self, x, W, b):
         return tf.nn.relu(tf.matmul(x, W) + b)
+
+    def pointwise_feed_forward_layer(self, x, W, b):
+
+        first_fc = self.fc(x, 
+        second_fc = self.fc(first_fc, tf.get_shape(first_fc[1]), 
+
+
+
+    def fc(self, x, num_in, num_out, name, relu=True):
+        '''Create a fully connected layer.
+           Adopted from Justin R. Fletcher'''
+        with tf.variable_scope(name) as scope:
+            # Create tf variables for the weights and biases
+            weights = tf.get_variable('weights', shape=[num_in, num_out],
+                            trainable=True)
+            biases = tf.get_variable('biases', [num_out], trainable=True)
+
+            # Matrix multiply weights and inputs and add bias
+            act = tf.nn.xw_plus_b(x, weights, biases, name=scope.name)
+
+        if relu:
+            # Apply ReLu non linearity
+            relu = tf.nn.relu(act)
+            return relu
+        else:
+            return act
 
     def attention(self, Q, K, V, mask=0):
         '''Implements attention layerA
